b18d84c6d36a204caa3fb8a97e75a4a09c41ba6b
Erik Sundell <erik.i.sundell@gmail.com>
2022-12-20 11:11:10 +0100

    Bump to 1.2.0.dev

--- autodoc_traits.py
+++ autodoc_traits.py
@@ -14,7 +14,7 @@ from traitlets import MetaHasTraits, TraitType, Undefined
 # __version__ should be updated using tbump, based on configuration in
 # pyproject.toml, according to instructions in RELEASE.md.
 #
-__version__ = "1.1.0"
+__version__ = "1.2.0.dev"
 
 
 class ConfigurableDocumenter(ClassDocumenter):
