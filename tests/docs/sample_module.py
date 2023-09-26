ca00840f5949a61e1ffc9ea9c647fa83a9315c7e
Erik Sundell <erik.i.sundell@gmail.com>
2023-09-26 09:44:44 +0200

    Add test to show help strings for other trait types
    I've obseved that Dict, List, Instance, and Command trait types no
    longer gets their help strings set with traitlets 5.10.0 but this wasn't
    an issue in traitlets 5.9.0.

--- tests/docs/sample_module.py
+++ tests/docs/sample_module.py
@@ -5,7 +5,7 @@ This module provides a module and classes with and without traits to test
 autodoc_traits against. It does not contain tests.
 """
 
-from traitlets import Bool
+from traitlets import Bool, Dict, Instance, Integer, Unicode, Union
 from traitlets.config.configurable import Configurable
 
 
@@ -81,3 +81,68 @@ class SampleNonConfigurableSubclass(SampleNonConfigurable):
 
     def subclass_method(self):
         """subclass_method docstring"""
+
+
+class TraitTypesSampleConfigurable(Configurable):
+    """TraitTypesSampleConfigurable docstring"""
+
+    trait_integer = Integer(
+        help="""trait_integer help text""",
+        config=True,
+    )
+    trait_integer_nohelp = Integer(
+        config=True,
+    )
+    trait_integer_noconfig = Integer(
+        help="""trait_integer_noconfig help text""",
+    )
+
+    trait_unicode = Unicode(
+        help="""trait_unicode help text""",
+        config=True,
+    )
+    trait_unicode_nohelp = Unicode(
+        config=True,
+    )
+    trait_unicode_noconfig = Unicode(
+        help="""trait_unicode_noconfig help text""",
+    )
+
+    trait_dict = Dict(
+        help="""trait_dict help text""",
+        config=True,
+    )
+    trait_dict_nohelp = Dict(
+        config=True,
+    )
+    trait_dict_noconfig = Dict(
+        help="""trait_dict_noconfig help text""",
+    )
+
+    trait_instance = Instance(
+        klass=SampleConfigurable,
+        help="""trait_instance help text""",
+        config=True,
+    )
+    trait_instance_nohelp = Instance(
+        klass=SampleConfigurable,
+        config=True,
+    )
+    trait_instance_noconfig = Instance(
+        klass=SampleConfigurable,
+        help="""trait_instance_noconfig help text""",
+    )
+
+    trait_union = Union(
+        [Integer(), Unicode()],
+        help="""trait_union help text""",
+        config=True,
+    )
+    trait_union_nohelp = Union(
+        [Integer(), Unicode()],
+        config=True,
+    )
+    trait_union_noconfig = Union(
+        [Integer(), Unicode()],
+        help="""trait_union_noconfig help text""",
+    )
--- tests/docs/source/autotrait/help.rst
+++ tests/docs/source/autotrait/help.rst
@@ -5,3 +5,18 @@ Test that we present the trait's provided ``help``.
 
 .. autotrait:: sample_module.SampleConfigurable.trait
    :noindex:
+
+.. autotrait:: sample_module.TraitTypesSampleConfigurable.trait_integer
+   :noindex:
+
+.. autotrait:: sample_module.TraitTypesSampleConfigurable.trait_unicode
+   :noindex:
+
+.. autotrait:: sample_module.TraitTypesSampleConfigurable.trait_dict
+   :noindex:
+
+.. autotrait:: sample_module.TraitTypesSampleConfigurable.trait_instance
+   :noindex:
+
+.. autotrait:: sample_module.TraitTypesSampleConfigurable.trait_union
+   :noindex:
